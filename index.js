8aaa79a4453b359356fd4577f3f91ca65a9342fb
rahul0901 <rahul.shinde0904@gmail.com>
2024-01-31 10:35:27 +0530

    adddd

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import route from './Routes/index.js';
+
+const app = express();
+dotenv.config();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', function(req,res){
+    res.send('Hi Backend!');
+});
+
+app.use('/api/v1', route);
+
+mongoose.connect(process.env.MongoUrl).then(()=>console.log('Database Connected!'));
+
+app.listen(8000, ()=>console.log('Port running on 8000'));
